103ed7476a55ef4185bd0eb69c6dbbb5a4a15cbf
emelcd <mick.altura@gmail.com>
2022-05-18 13:09:16 +0200

    init Commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,48 +1,50 @@
-import React, { useEffect } from "react";
-import "./App.css";
-import socket from "./utils/socket";
-import { GoogleIdentity } from "./components/GoogleIdentity";
+import React, { useEffect } from 'react'
+import './App.css'
+import socket from './utils/socket'
+import { GoogleIdentity } from './components/GoogleIdentity'
 
 const CLIENT_ID =
-    "559293673854-9nc9u8ml9jeie373g1mg62it3q2r0bdv.apps.googleusercontent.com";
+    '559293673854-9nc9u8ml9jeie373g1mg62it3q2r0bdv.apps.googleusercontent.com'
 
-socket.on("connect", () => {
-    console.log("connected");
-});
+socket.on('connect', () => {
+  console.log('connected')
+})
 
-function App() {
-    const [token, setToken]: [string, Function] = React.useState("");
-    const saveToken = (token: string) => {
-        const localToken = localStorage.getItem("token");
-        if (!localToken) {
-            localStorage.setItem("token", token);
-            setToken(token);
-            socket.emit("joinToken", token);
-        } else {
-            localStorage.removeItem("token");
-            localStorage.setItem("token", token);
-            setToken(token);
-        }
-    };
+function App () {
+  const [token, setToken]: [string, Function] = React.useState('')
+  const saveToken = (token: string) => {
+    const localToken = localStorage.getItem('token')
+    if (!localToken) {
+      localStorage.setItem('token', token)
+      setToken(token)
+      socket.emit('joinToken', token)
+    } else {
+      localStorage.removeItem('token')
+      localStorage.setItem('token', token)
+      setToken(token)
+    }
+  }
 
-    return (
+  return (
         <>
-            {token ? (
+            {token
+              ? (
                 <>{token}</>
-            ) : (
+                )
+              : (
                 <>
                     <GoogleIdentity
                         onSignIn={saveToken}
                         onSignOut={() => {
-                            localStorage.removeItem("token");
-                            setToken("");
+                          localStorage.removeItem('token')
+                          setToken('')
                         }}
                         clientId={CLIENT_ID}
                     />
                 </>
-            )}
+                )}
         </>
-    );
+  )
 }
 
-export default App;
+export default App
